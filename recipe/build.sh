d717dd5db4f041aadd5fee62518db14032675785
Wes McKinney <wesm@users.noreply.github.com>
2016-10-10 18:05:39 -0400

    Link boost symbols statically (#2)

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,7 @@ cd build-dir
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DARROW_BOOST_USE_SHARED=off \
     -DARROW_BUILD_BENCHMARKS=off \
     -DARROW_BUILD_TESTS=off \
     -DARROW_HDFS=on \
