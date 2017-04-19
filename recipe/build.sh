80ce916ceb71e2252904bb2e56fd9bf437a8fc19
Wes McKinney <wes.mckinney@twosigma.com>
2017-04-18 23:09:13 -0400

    Set explicit libdir on UNIX to avoid multiarch issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,6 +14,7 @@ cd build-dir
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
     -DARROW_BOOST_USE_SHARED=off \
     -DARROW_BUILD_BENCHMARKS=off \
     -DARROW_BUILD_TESTS=off \
