f38ddbbaa4147be31ff665b96f35819e9f16f18c
Wes McKinney <wes.mckinney@twosigma.com>
2017-07-17 09:13:15 -0400

    Update to latest, add zstd/lz4-c dependencies

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,6 @@ cmake -G "Visual Studio 14 2015 Win64" ^
       -DARROW_BOOST_USE_SHARED:BOOL=OFF ^
       -DARROW_BUILD_TESTS:BOOL=OFF ^
       -DARROW_BUILD_UTILITIES:BOOL=OFF ^
-      -DARROW_ZLIB_VENDORED:BOOL=OFF ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DARROW_PYTHON=on ^
       ..
