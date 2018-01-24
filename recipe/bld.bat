8242cc80c2b7cd1bd4fccc184db1e14b3d2586e8
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2018-01-24 23:06:36 +0100

    Use environment variables for VS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ pushd "%SRC_DIR%"\cpp\build
 
 set ARROW_BUILD_TOOLCHAIN=%LIBRARY_PREFIX%
 
-cmake -G "Visual Studio 14 2015 Win64" ^
+cmake -G "Visual Studio %VS_MAJOR% %VS_YEAR% Win64" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DARROW_BOOST_USE_SHARED:BOOL=OFF ^
       -DARROW_BUILD_TESTS:BOOL=OFF ^
