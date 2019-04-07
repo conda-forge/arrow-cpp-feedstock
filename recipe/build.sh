ac768c5480d8b63d7ccc8bb3bce2e8cc487a8948
Uwe L. Korn <uwelk@xhochy.com>
2019-04-07 12:55:05 +0200

    Lookup system include paths using gcc-bash-magic

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,14 @@ set -x
 mkdir cpp/build
 pushd cpp/build
 
+EXTRA_CMAKE_ARGS=""
+
+# Include g++'s system headers
+if [ "$(uname)" == "Linux" ]; then
+  SYSTEM_INLUDES=$(echo | ${CXX} -E -Wp,-v - 2>&1 | grep '^ ' | awk '{print "-isystem;" substr($1, 1)}' | tr '\n' ';')
+  EXTRA_CMAKE_ARGS=" -DARROW_GANDIVA_PC_CXX_FLAGS=\"${SYSTEM_INLUDES}\""
+fi
+
 cmake \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -28,7 +36,7 @@ cmake \
     -DCMAKE_RANLIB=${RANLIB} \
     -DPYTHON_EXECUTABLE="${PREFIX}/bin/python" \
     -GNinja \
-    ..
+    ${EXTRA_CMAKE_ARGS} ..
 
 ninja install
 
