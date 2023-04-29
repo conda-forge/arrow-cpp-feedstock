d98af91a7cbf1dc083eff6f3c6b2c3b178c9b61a
Ben Mares <services-git-throwaway1@tensorial.com>
2023-04-29 15:57:03 +0200

    Quote strings

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -8,23 +8,23 @@
 # is benign and doesn't need to be deleted.
 
 # where the GDB wrappers get installed
-GDB_PREFIX=$CONDA_PREFIX/share/gdb/auto-load
+GDB_PREFIX="$CONDA_PREFIX/share/gdb/auto-load"
 
 # If the directory is not writable, nothing can be done
-if [ ! -w $GDB_PREFIX ]; then
+if [ ! -w "$GDB_PREFIX" ]; then
     return
 fi
 
 # this needs to be in sync with the respective patch
-PLACEHOLDER=replace_this_section_with_absolute_slashed_path_to_CONDA_PREFIX
+PLACEHOLDER="replace_this_section_with_absolute_slashed_path_to_CONDA_PREFIX"
 # the paths here are intentionally stacked, see #935, resp.
 # https://github.com/apache/arrow/blob/master/docs/source/cpp/gdb.rst#manual-loading
-WRAPPER_DIR=$GDB_PREFIX/$CONDA_PREFIX/lib
+WRAPPER_DIR="$GDB_PREFIX/$CONDA_PREFIX/lib"
 
-mkdir -p $WRAPPER_DIR
+mkdir -p "$WRAPPER_DIR"
 # there's only one lib in that folder, but the libname changes
 # based on the version so use a loop instead of hardcoding it.
-for f in $GDB_PREFIX/$PLACEHOLDER/lib/*.py; do
+for f in "$GDB_PREFIX/$PLACEHOLDER/lib/*.py"; do
     # overwrite, because we don't have deactivation (i.e. symlink remains)
-    ln -sf $f $WRAPPER_DIR/$(basename $f)
+    ln -sf "$f" "$WRAPPER_DIR/$(basename $f)"
 done
