d1e3e44fb04b037665ec17e27159f77c01bd4005
Raúl Cumplido <raulcumplido@gmail.com>
2023-05-02 12:39:00 +0200

    Enable ARROW_ACERO

--- recipe/build-arrow.sh
+++ recipe/build-arrow.sh
@@ -74,9 +74,10 @@ fi
 export READ_RECIPE_META_YAML_WHY_NOT=OFF
 
 # for available switches see
-# https://github.com/apache/arrow/blame/apache-arrow-11.0.0/cpp/cmake_modules/DefineOptions.cmake
+# https://github.com/apache/arrow/blame/apache-arrow-12.0.0/cpp/cmake_modules/DefineOptions.cmake
 # placeholder in ARROW_GDB_INSTALL_DIR must match what's used for replacement in activate.sh
 cmake -GNinja \
+    -DARROW_ACERO=ON \
     -DARROW_BOOST_USE_SHARED=ON \
     -DARROW_BUILD_BENCHMARKS=OFF \
     -DARROW_BUILD_STATIC=OFF \
