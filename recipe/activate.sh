8719e27bb3581bc4b44ec6f8d5d5013d51fbca82
Ben Mares <services-git-throwaway1@tensorial.com>
2025-01-24 13:18:39 -0800

    Bail on activation when CONDA_BUILD is set (#1694)
    Use CONDA_BUILD_STATE to prevent bailing on test

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -18,6 +18,15 @@ _la_log() {
     fi
 }
 
+# Skip activation if CONDA_BUILD environment variable is set.
+# (CONDA_BUILD is also set in the test stage, and we don't want to skip there.)
+# Otherwise, the symlinks will be included in packages built with libarrow as a host dependency.
+# see https://github.com/conda-forge/arrow-cpp-feedstock/issues/1478
+if [ -n "$CONDA_BUILD" ] && [ "${CONDA_BUILD_STATE:-0}" != "TEST" ]; then
+    _la_log "CONDA_BUILD is set to $CONDA_BUILD (and CONDA_BUILD_STATE != \"TEST\"), skipping libarrow activation."
+    return 0
+fi
+
 _la_log "Beginning libarrow activation."
 
 # where the GDB wrappers get installed
