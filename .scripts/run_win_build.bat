94877e216f608a65071f509c51b7828fe4e340ee
H. Vetinari <h.vetinari@gmx.com>
2026-01-24 19:56:01 +1100

    MNT: Re-rendered with conda-smithy 3.54.1 and conda-forge-pinning 2026.01.24.04.42.29
    Other tools:
    - conda-build 25.11.1
    - rattler-build 0.55.0
    - rattler-build-conda-compat 1.4.10

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -107,17 +107,13 @@ if /i "%CI%" == "azure" (
 )
 
 :: Validate
-call :start_group "Validating outputs"
-validate_recipe_outputs "%FEEDSTOCK_NAME%"
-if !errorlevel! neq 0 exit /b !errorlevel!
-call :end_group
 
 if /i "%UPLOAD_PACKAGES%" == "true" (
     if /i "%IS_PR_BUILD%" == "false" (
         call :start_group "Uploading packages"
         if not exist "%TEMP%\" md "%TEMP%"
         set "TMP=%TEMP%"
-        upload_package --validate --feedstock-name="%FEEDSTOCK_NAME%" .\ ".\recipe" .ci_support\%CONFIG%.yaml
+        upload_package  .\ ".\recipe" .ci_support\%CONFIG%.yaml
         if !errorlevel! neq 0 exit /b !errorlevel!
         call :end_group
     )
